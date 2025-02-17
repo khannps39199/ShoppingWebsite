6da99c91e19b964455bfa8c972d70369ee28b15e
khannps39199 <khannps39199@gmail.com>
2025-02-17 15:09:36 +0700

    commit before pull

--- src/main/java/poly/edu/Controller/OderController.java
+++ src/main/java/poly/edu/Controller/OderController.java
@@ -2,11 +2,16 @@ package poly.edu.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
 import poly.edu.Repository.*;
 import poly.edu.Service.CookieService;
 import poly.edu.Service.ParamService;
 import poly.edu.Service.SessionService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class OderController {
@@ -20,4 +25,14 @@ public class OderController {
 	OrderRepository orderRepo;
 	@Autowired
 	OrderDetailsRepository odertDetailRepo;
+	@GetMapping("/order")
+	public String getOrder(Model model) {
+		model.addAttribute("Component","UsersOrder.html");
+		return "UserLayout";
+	}
+	@PostMapping("/order")
+	public String insertOrder(Model model) {
+		model.addAttribute("Component","UsersOrder.html");
+		return "redirect:/order";
+	}
 }
