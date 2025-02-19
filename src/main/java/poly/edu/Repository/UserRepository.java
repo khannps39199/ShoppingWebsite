b648cbab77c08e0028fcfdc74edd3f5698ff6d66
Khoanhps39175 <Khoanhps39175@gmail.com>
2025-02-19 19:20:53 +0700

    Backend Đăng Ký

--- src/main/java/poly/edu/Repository/UserRepository.java
+++ src/main/java/poly/edu/Repository/UserRepository.java
@@ -1,9 +1,20 @@
 package poly.edu.Repository;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import poly.edu.Entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
+	Page<User> findAll(Pageable pageable);
+	 @Query("SELECT u FROM User u WHERE u.username = :name")
+	   Optional<User> findByUsername(@Param("name") String name);
+	 @Query("SELECT u FROM User u WHERE u.email = :email")
+	   Optional<User> findByEmail(@Param("email") String email);
 
 }
