85e2d510cdd9c58ed2dcf723f789cfcd3bab8694
khannps39199 <khannps39199@gmail.com>
2025-04-13 19:49:50 +0700

    retry to run tets with testNG

--- src/test/java/poly/edu/Ps39199NguyenNhutKhaAssignmentApplicationTests.java
+++ src/test/java/poly/edu/Ps39199NguyenNhutKhaAssignmentApplicationTests.java
@@ -12,22 +12,5 @@ class Ps39199NguyenNhutKhaAssignmentApplicationTests {
     @Test
     void contextLoads() {
     }
-//	@Test
-//	public void testIsEvenNumber2() {
-//		app tempApp=new app();
-//		boolean result= tempApp.isEvenNum(2);
-//		assertTrue(result);
-//	}
-//	@Test
-//	public void testIsEvenNumber3() {
-//		app tempApp=new app();
-//		boolean result= tempApp.isEvenNum(3);
-//		assertTrue(result);
-//	}
-//	@Test
-//	public void testIsEvenNumber4() {
-//		app tempApp=new app();
-//		boolean result= tempApp.isEvenNum(4);
-//		assertTrue(result);
-//	}
+
 }
